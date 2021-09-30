3ae699e2bfedb1b12d290960d382cbe676b156e4
Moises Sacal <moisbo@gmail.com>
2021-09-30 15:02:41 +1000

    fixed license, select only from group and slug, changed solr query to send encoded component

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,41 @@
 const github = require('../services/Github');
+const _ = require('lodash');
 
 function authorize({config, url, session}) {
 
   let filterQuery = [];
+  const group = config['auth']['group'];
   if (session['memberships'] && session['memberships']['teams']) {
     const sessionTeams = session['memberships']['teams'];
     const teams = config['auth']['teams'];
     for (let t of teams) {
       const found = sessionTeams.find(function (tt) {
-        return tt['team']['slug'] === t;
+        const org = tt['team']['organization'] || null;
+        if (org && org['login'] === group) {
+          return tt['team']['slug'] === t;
+        }
       });
       if (found) {
-        filterQuery.push(`${t}`)
+        filterQuery.push(`${t}`);
       }
     }
-    const filterJoin = ',' + filterQuery.join(',');
-    const base = config['solr_fq'];
-    const mod = url + '&fq=' + base + filterJoin;
+    const fq = [];
+    _.each(filterQuery, (filter) => {
+      return _.find(config['auth']['licenses'], (l) => {
+        if (l['team'] === filter) {
+          fq.push(`"${encodeURIComponent(l['license'])}"`);
+        }
+      })
+    });
+    let filter = '';
+    const base = config['solr_fq'] || '_license';
+    if (fq.length > 0) {
+      const filterJoin = fq.join(' OR ');
+      filter = base + '%3A("Public"%20OR%20' + filterJoin + ')'
+    } else {
+      filter = base + '%3APublic';
+    }
+    const mod = url + '&fq=' + filter;
     return mod;
   } else {
     return url + '&fq=' + config['solr_fq']
